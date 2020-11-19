471d45441ed638e2455dc93847d86a4b9a3b5c8a
josimarminetefera <josimaminete@gmail.com>
2020-11-18 22:31:28 -0300

    Melhorias de layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,36 +10,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("MyApp Criando primeiro ScopedModel da aplicação.");
-    return ScopedModel<UsuarioModel>( //este scoped controla toda a aplicação pela class user_model
+    return ScopedModel<UsuarioModel>(
+        //este scoped controla toda a aplicação pela class user_model
 
-      //tem que ser especificado o modelo
-      model: UsuarioModel(),// tudo que estiver dentro deste scope pode ser acessado e modificado pelo usermodel
+        //tem que ser especificado o modelo
+        model: UsuarioModel(), // tudo que estiver dentro deste scope pode ser acessado e modificado pelo usermodel
 
-      //quando trocar de usuário eu quero que o carrinho seja atualizado por isso o decendant
-      child: ScopedModelDescendant<UsuarioModel>(
-        builder: (context, child, model){
-          print("--MyApp iniciando a apllicação.");
-          return ScopedModel<CarrinhoModel>(// o carrinho precisa saber o usuário que ele pertence por isso ele ficou dentro do usermodel
+        //quando trocar de usuário eu quero que o carrinho seja atualizado por isso o decendant
+        child: ScopedModelDescendant<UsuarioModel>(
+          builder: (context, child, model) {
+            print("--MyApp iniciando a apllicação.");
+            return ScopedModel<CarrinhoModel>(
+              // o carrinho precisa saber o usuário que ele pertence por isso ele ficou dentro do usermodel
 
-            model: CarrinhoModel(model),
+              model: CarrinhoModel(model),
 
-            child: MaterialApp(
-              //onde é iniciando o meu app de verdade
-              title: 'Flutter',
-              theme: ThemeData(
-                  primarySwatch: Colors.red, //cor inicial
-                  primaryColor: Color.fromARGB(255, 4, 125, 141)), //Cor principal do aplicativo
-              debugShowCheckedModeBanner: false, //negocio que fica no canto direito
+              child: MaterialApp(
+                //onde é iniciando o meu app de verdade
+                title: 'Flutter',
+                theme: ThemeData(
+                    primarySwatch: Colors.red, //cor inicial
+                    primaryColor: Color.fromARGB(255, 4, 125, 141)), //Cor principal do aplicativo
+                debugShowCheckedModeBanner: false, //negocio que fica no canto direito
 
-
-              //tela principal que inicia todos componentes
-              home: HomeScreen(),
-
-
-            ),
-          );
-        },
-      )
-    );
+                //tela principal que inicia todos componentes
+                home: HomeScreen(),
+              ),
+            );
+          },
+        ));
   }
 }
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -12,37 +12,37 @@ class CustomDrawer extends StatelessWidget {
   //o custronDrawer precisará acessar o _pageController do home scean para acessar paginas quando clicar em alguma
   //das opções de botões da tela do navegation
   // na verdade deverá ser chamado a ação de trocar a tela pelos botoes que ficam la dentro do drawerTile
-  final PageController pageController;//para mudar de tela quando clicado em um item de menu
+  final PageController pageController; //para mudar de tela quando clicado em um item de menu
   //controlador para receber o page controller
   CustomDrawer(this.pageController);
 
   @override
   Widget build(BuildContext context) {
-
     print("--CustomDrawer iciando montagem do drawer");
     //retorna um fundo com degrade
-    Widget _buildDrawerBack() => Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(100, 211, 118, 130),
-                Color.fromARGB(100, 253, 181, 168),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter
-        )
-      ),
-    );
+    Widget _buildDrawerBack() =>
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(100, 211, 118, 130),
+                    Color.fromARGB(100, 253, 181, 168),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter
+              )
+          ),
+        );
 
     return Drawer( //o que monta a tela
       child: Stack( //colocar um fundo sobre o drawer
         children: <Widget>[
-          _buildDrawerBack(),//fundo com degrade
+          _buildDrawerBack(), //fundo com degrade
 
           ListView( //lista de opções do menu lateral
-            padding: EdgeInsets.only(left: 32.0, top: 16.0),//alinhamento para todos itens
+            padding: EdgeInsets.only(left: 32.0, top: 16.0), //alinhamento para todos itens
             children: <Widget>[
-              Container(//conteudo da tela superior com as opções
+              Container( //conteudo da tela superior com as opções
                 margin: EdgeInsets.only(bottom: 8.0),
                 padding: EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 8.0),
                 height: 170.0,
@@ -54,61 +54,62 @@ class CustomDrawer extends StatelessWidget {
                       top: 8.0,
                       left: 0.0,
                       child: Text(
-                          "Nome da \nAplicação",//com quebra de linha
-                          style: TextStyle(fontSize: 34.0,
-                          fontWeight: FontWeight.bold
+                        "Nome da \nAplicação", //com quebra de linha
+                        style: TextStyle(fontSize: 34.0,
+                            fontWeight: FontWeight.bold
                         ),
                       ),
                     ),
 
                     //itens que vão ficar na parte de baixo do titulo
                     Positioned(
-                      left: 0.0,
-                      bottom: 0.0,
+                        left: 0.0,
+                        bottom: 0.0,
 
-                      child: ScopedModelDescendant<UsuarioModel>(
-                        builder: (context, child, model){
-                          print("CustonDrawer ScopedModelDescendant para o texto olá e sair ou entrar.");
-                          return Column(// coluna porque um texto vai ficar ensima do outro
-                            crossAxisAlignment: CrossAxisAlignment.start, //alinhar no inicio
-                            children: <Widget>[
+                        child: ScopedModelDescendant<UsuarioModel>(
+                          builder: (context, child, model) {
+                            print("CustonDrawer ScopedModelDescendant para o texto olá e sair ou entrar.");
+                            return Column( // coluna porque um texto vai ficar ensima do outro
+                              crossAxisAlignment: CrossAxisAlignment.start, //alinhar no inicio
+                              children: <Widget>[
 
-                              Text(
-                                "Olá, ${model.estaLogado()?model.dadosUsuario["nome"]:""}",
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-
-                              GestureDetector( //para fazer capturar o click
-                                child: Text(
-                                  model.estaLogado()? "Sair" : "Entre ou Cadastre",
+                                Text(
+                                  "Olá, ${model.estaLogado() ? model.dadosUsuario["nome"] : ""}",
                                   style: TextStyle(
-                                      color: Theme.of(context).primaryColor,//para pegar a cor principal do app
-                                      fontSize: 16.0,
+                                      fontSize: 18.0,
                                       fontWeight: FontWeight.bold
                                   ),
                                 ),
-                                onTap: (){
-                                  if(model.estaLogado()){
-                                    model.sair();
-                                  }else {
-                                    //para abrir uma nova tela da aplicação sobre outra
-                                    //para subistituir uma tela usa o recurso da tela de login cadastrar
-                                    Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                            builder: (context) => LoginScreen())
-                                    );
-                                  }
-                                },
-                              )
 
-                            ],
-                          );
+                                GestureDetector( //para fazer capturar o click
+                                  child: Text(
+                                    model.estaLogado() ? "Sair" : "Entre ou Cadastre",
+                                    style: TextStyle(
+                                        color: Theme
+                                            .of(context)
+                                            .primaryColor, //para pegar a cor principal do app
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                  onTap: () {
+                                    if (model.estaLogado()) {
+                                      model.sair();
+                                    } else {
+                                      //para abrir uma nova tela da aplicação sobre outra
+                                      //para subistituir uma tela usa o recurso da tela de login cadastrar
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) => LoginScreen())
+                                      );
+                                    }
+                                  },
+                                )
 
-                        },
-                      )
+                              ],
+                            );
+                          },
+                        )
                     )
 
                   ],
